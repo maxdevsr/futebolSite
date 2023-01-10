5887c027ef2cd27f10307133778bc39bba7898a6
macsonsr <macson.rocha@camptecnologia.com.br>
2023-01-10 09:54:22 -0300

    fix: att imagens header

--- futebol-site/src/components/header/index.jsx
+++ futebol-site/src/components/header/index.jsx
@@ -1,18 +1,28 @@
 import Logo from "../../assets/logo_topo.png"
-
+import PatchPremier from "../../assets/premier.png"
+import PatchSerieA from "../../assets/Serie_A.png"
+import PatchLigue1 from "../../assets/ligue1.svg"
+import PatchBundesliga from "../../assets/bundesliga.svg"
+import PatchEredivisie from "../../assets/eredivisie.svg"
+import PatchLigaNOS from "../../assets/LigaNOS.png"
+import PatchSupArg from "../../assets/ligaArgentina.png"
+import PatchBrasileirao from "../../assets/brasileiro.svg"
+import PatchLigaMX from "../../assets/mexican.png"
+import PatchMLS from "../../assets/MLS.svg"
+import PatchLaliga from "../../assets/laliga.svg"
 function Header() {
     const leagues = [
-        { nome: 'Premier League', pais: 'Inglaterra' },
-        { nome: 'La Liga', pais: 'Espanha' },
-        { nome: 'Serie A', pais: 'Itália' },
-        { nome: 'Ligue 1', pais: 'França' },
-        { nome: 'Bundesliga', pais: 'Alemanha' },
-        { nome: 'Eredivisie', pais: 'Holanda' },
-        { nome: 'Primeira Liga', pais: 'Portugal' },
-        { nome: 'Superliga Argentina', pais: 'Argentina' },
-        { nome: 'Campeonato Brasileiro', pais: 'Brasil' },
-        { nome: 'Liga MX', pais: 'México' },
-        { nome: 'Major League Soccer', pais: 'Estados Unidos' },
+        { nome: 'Premier League', pais: 'Inglaterra', imagem: PatchPremier },
+        { nome: 'La Liga', pais: 'Espanha', imagem: PatchLaliga },
+        { nome: 'Serie A', pais: 'Itália', imagem: PatchSerieA },
+        { nome: 'Ligue 1', pais: 'França', imagem: PatchLigue1 },
+        { nome: 'Bundesliga', pais: 'Alemanha', imagem: PatchBundesliga },
+        { nome: 'Eredivisie', pais: 'Holanda', imagem: PatchEredivisie },
+        { nome: 'Primeira Liga', pais: 'Portugal', imagem: PatchLigaNOS },
+        { nome: 'Liga Argentina', pais: 'Argentina', imagem: PatchSupArg },
+        { nome: 'Brasileirão', pais: 'Brasil', imagem: PatchBrasileirao },
+        { nome: 'Liga MX', pais: 'México', imagem: PatchLigaMX },
+        { nome: 'M.L.S', pais: 'Estados Unidos', imagem: PatchMLS },
     ]
     return (
         <header>
@@ -24,7 +34,7 @@ function Header() {
                 <span>Login</span>
             </div>
             <ul className="subHeader">
-                {leagues.map((league, key) => <li key={key}>{league.nome}</li>)}
+                {leagues.map((league, key) => <li key={key}> <img src={league.imagem} alt="" /> <span> {league.nome}</span></li>)}
             </ul>
         </header>
     )
